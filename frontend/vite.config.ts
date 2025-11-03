5e4f38c8db66e6211a785a2d535cf558eec99527
mlow <marcuslowsj@gmail.com>
2025-11-03 08:10:45 +0000

    testing website

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: "/Pomodoro_Timer_LuoLuo/",
+  base: "./",
   plugins: [
     tailwindcss(),
   ],
